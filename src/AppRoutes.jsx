81c43e3400b3d5f8083066026f4b6e2a03bbc90c
boody <bodymagdy97@gmail.com>
2025-07-19 16:45:33 +0300

    scdc

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -2,12 +2,12 @@ import { Routes, Route } from "react-router-dom";
 
 import React from 'react'
 import Home from "./pages/Home";
-import About from "./pages/About";
+import Projects from "./pages/Projects";
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/Projects" element={<Projects />} />
     </Routes>
   )
 }
